0d94e9e1683f55b8b25b9003a00ddc2f093d8b19
10thfloor <remember.mackenzie@gmail.com>
2021-02-22 08:01:47 -0800

    Top Nav.

--- layouts/default.js
+++ layouts/default.js
@@ -5,9 +5,8 @@ import React, { useContext } from "react";
 import xw from "xwind";
 
 import { RiMoonLine, RiSunLine } from "react-icons/ri";
-import { GoTools } from "react-icons/go";
-import { IoHelpBuoy, IoHome } from "react-icons/io5";
 import { FaDiscord, FaTwitter, FaDiscourse } from "react-icons/fa";
+import { BsChatDots } from "react-icons/bs";
 
 import { ThemeContext } from "../components/ThemeContext";
 import { Logo } from "../components/Logo";
@@ -42,7 +41,7 @@ const Layout = ({ children }) => {
               <h4
                 css={xw`text-xs text-gray-400 dark[text-gray-600] pb-2 tracking-tight`}
               >
-                <div css={xw`flex items-center `}>DEVELOPER TOOLS</div>
+                <div css={xw`flex items-center`}>DEVELOPER TOOLS</div>
               </h4>
               <ul>
                 <li>Cadence</li>
@@ -69,17 +68,40 @@ const Layout = ({ children }) => {
         <main css={xw`w-main overflow-x-hidden`}>
           <div css={xw`overflow-x-hidden overflow-y-scroll h-screen`}>
             <header
-              css={xw`flex absolute w-main h-20 z-10 bg-white dark:bg-gray-900`}
+              css={xw`flex absolute w-main z-10 bg-white dark:bg-gray-900 border-b border-gray-100 dark[border-gray-800 text-white]`}
             >
-              <button
-                css={xw`flex justify-center pl-10 h-full items-center`}
-                type="button"
-                onClick={() => setTheme(isDark ? "light" : "dark")}
-              >
-                <span css={xw`text-sm dark:text-indigo-400`}>
-                  {isDark ? <RiSunLine /> : <RiMoonLine />}
-                </span>
-              </button>
+              <div css={xw`flex flex-col flex-grow`}>
+                <ul
+                  css={xw`flex h-16 font-semibold tracking-tight items-center space-x-10 pl-10  border-b border-gray-200 dark[border-green-800]`}
+                >
+                  <li>Acquire & Use Flow Tokens</li>
+                  <li>Build Experiences on Flow</li>
+                  <li css={xw`flex-grow`}>Join The Network</li>
+                  <div css={xw`text-sm flex items-center`}>
+                    <div css={xw`pr-2`}>Feedback</div>
+                    <BsChatDots />
+                  </div>
+                  <button
+                    css={xw`pr-10 flex justify-center pl-10 h-full items-center`}
+                    type="button"
+                    onClick={() => setTheme(isDark ? "light" : "dark")}
+                  >
+                    <span css={xw`text-sm dark:text-indigo-400`}>
+                      {isDark ? <RiSunLine /> : <RiMoonLine />}
+                    </span>
+                  </button>
+                </ul>
+                <ul
+                  css={xw`text-sm h-10 flex flex-grow items-center space-x-8 pl-10`}
+                >
+                  <li css={xw`p-2 pl-0`}>Overview</li>
+                  <li css={xw`p-2`}>Reference</li>
+                  <li css={xw`p-2`}>Guides</li>
+                  {/* <li>Samples</li>
+                  <li>Support</li> */}
+                  <li css={xw`p-2`}>Resources</li>
+                </ul>
+              </div>
             </header>
             <div css={xw`w-full flex`}>
               <div
